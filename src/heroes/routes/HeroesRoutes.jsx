a57bdb05b9b30b5e52849fb0e0d38ec8887a22f5
Valeria <valelopezc4@gmail.com>
2023-05-08 14:12:57 -0400

    initial

--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -7,8 +7,16 @@ export const HeroesRoutes = () => {
     
         <Navbar />
 
-    
-    
+        <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
+
+        <Route path="login" element={<LoginPage />} />
+
+        <Route path="/" element={<Navigate to="/marvel" />} />
+
+    </Routes>
+
     </>
     )
 }
